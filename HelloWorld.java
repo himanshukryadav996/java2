349209f42e0628d63739d1ba6ce01833873c9ad9
himanshukryadav996 <himanshukryadav996@gmail.com>
2023-04-05 15:25:53 -0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+    public static void HelloWorld() {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    
+    }
+    public static  int sum( int a , int b) {
+        a = 6;
+        b = 8;
+        int c = a+b;
+       System.out.println("The sum of is" + c);
+
+       //int c = a+b;
+        return 2;
+        
+    }
+    public static void main(String[] args) {
+      int a;
+        int b;
+        // HelloWorld();
+        sum( a,b);
+
+   
+    }
+    
+}
